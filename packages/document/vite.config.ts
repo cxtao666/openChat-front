58d94eaa17a71d929f1c779a0f6b8db6aa99e186
cxtao <1246784448@qq.com>
2023-10-09 11:58:44 +0800

    feat: 处理vite打包路径

--- packages/document/vite.config.ts
+++ packages/document/vite.config.ts
@@ -3,25 +3,30 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import qiankun from "vite-plugin-qiankun"
-const isProd = process.env.NODE_ENV === "production";
+import qiankun from 'vite-plugin-qiankun'
+const isProd = process.env.NODE_ENV === 'production'
+
+// 自己写个vite插件对链接进行处理
+const addPrefixPlugin = (prefix) => ({
+  name: 'add-prefix-plugin',
+  async transformIndexHtml(html) {
+    return html.replace(/(href|src)="(?!http|\/\/)/g, `$1="${prefix}`).replace(/import\('(.+)'\)/, `import('${prefix}$1')`);
+  }
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    assetsDir: 'assets', // 资源文件的输出目录
-    assetsPublicPath: '/doc/', // 资源文件的 URL 前缀
-  },
   plugins: [
     vue(),
     vueJsx(),
     qiankun('doc', {
       useDevMode: true
-    })
+    }),
+    addPrefixPlugin(isProd ? '/doc' : '')
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
+  }
 })
