9825fe004f6269c90845155ed8bc92f4903ef49c
caixiantao <caixiantao.123@bytedance.com>
2023-10-11 14:15:35 +0800

    feat: 添加脚本

---
+++ sh/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 定义子应用目录列表
+cd packages
+sub_apps=$(find . -maxdepth 1 -type d)
+
+char='.'
+
+# 遍历子应用目录并构建
+for app in $sub_apps
+do
+  if [ "$app" = "$char" ]; then 
+    continue
+  fi
+  echo "Building $app"
+  cd "$app"
+  npm run build
+  cd ..
+done
+
+echo "All apps have been build"
