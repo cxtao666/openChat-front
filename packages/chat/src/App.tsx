2652d5d3075548339b645005bcdaaf24d51488c2
cxtao <1246784448@qq.com>
2024-01-01 17:52:20 +0800

    feat: 删除引用

--- packages/chat/src/App.tsx
+++ packages/chat/src/App.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Index from "./pages/Index";
-import Login from "./pages/Login";
 import { BrowserRouter, Route } from "react-router-dom";
-import Register from "pages/Register";
-import { ErrorPage } from 'components/error';
 import "antd/dist/antd.css";
 
 // 组件必须以大写字母开头，否则TypeScript会大喊大叫
